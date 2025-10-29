30f763cb29693ee292eb2492e8e6cdb1e4e4a5e0
sanchezvalenciamarcos462-dev <sanchezvalenciamarcos462@gmail.com>
2025-10-29 10:06:56 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
     <h1>Mi Web Frontend</h1>
     <nav>
       <ul>
-        <li><a href="Inicio.php">Inicio</a></li>
+        <li><a href="index.php">Inicio</a></li>
         <li><a href="acerca.php">Acerca</a></li>
         <li><a href="contacto.php">Contacto</a></li>
       </ul>
